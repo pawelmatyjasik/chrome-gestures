cdaea1395b3f6f8b6998af8863ce73ed91fdc8d7
Pawel Matyjasik <pawel_matyjasik@epam.com>
2019-02-04 19:13:13 +0100

    fix: correct forward swipe recognition function

--- src/gestures.js
+++ src/gestures.js
@@ -2,6 +2,6 @@ export function isHorizontalSwipe(deltaX, deltaY) {
     return Math.abs(deltaY) < 0.1 && Math.abs(deltaX) > 1;
 }
 
-export function isForward() {
-    return e.deltaX > 0;
+export function isForward(deltaX) {
+    return deltaX > 0;
 }
